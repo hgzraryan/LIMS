b01c9950458673a11638bf184ee6fe564d827bc9
Hayk Harutyunyan <haykprog0720@gmail.com>
2023-06-13 00:39:53 +0400

    added modal component

--- src/components/views/Agents.js
+++ src/components/views/Agents.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from './../Modal'
 
 export default function Agents() {
+  const [show,setShow] = useState(true)
   return (
     <div>
-      Agents:
+      <h2>Click to open Modal</h2>
+      <button onClick={() => setShow(true)}>Open Modal</button>
+      <Modal show = {show} closeModal={() => setShow(false)}>
+          Agents:0
+      </Modal>
     </div>
   )
 }
