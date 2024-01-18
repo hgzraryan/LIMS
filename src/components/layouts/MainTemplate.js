8e83aa86df9a93512d2e88d2e11ca96a21fb79db
Hayk Harutyunyan <haykprog0720@gmail.com>
2024-01-18 19:37:34 +0400

    recover logout button

--- src/components/layouts/MainTemplate.js
+++ src/components/layouts/MainTemplate.js
@@ -96,7 +96,201 @@ const MainTemplate = () => {
                 {/* Top Navbar */}
                 
               {/* /Top Navbar */}
-  
+  <nav className="hk-navbar navbar navbar-expand-xl navbar-light fixed-top">
+                    <div className="container-fluid">
+                    {/* Start Nav */}
+                        <div className="nav-start-wrap">
+                            <button className="btn btn-icon btn-rounded btn-flush-dark flush-soft-hover navbar-toggle d-xl-none" onClick={mmenuClick}>
+                                <span className="icon">
+                                    <span className="feather-icon">
+                                        {/*<i data-feather="align-left"></i>*/}
+                                        <i className="fas fa-align-left"></i>
+                                    </span>
+                                </span>
+                            </button>
+                            {/* Search */}
+                            
+                            {/* /Search */}
+                        </div>
+                        {/* /Start Nav */}
+                        {/* End Nav */}
+                        <div className="nav-end-wrap" onClick={menuClick}>
+                            <ul className="navbar-nav flex-row">
+                            {/*
+                                <li className="nav-item">
+                                    <a href="email.html" className="btn btn-icon btn-rounded btn-flush-dark flush-soft-hover"><span className="icon"><span className=" position-relative"><span className="feather-icon"><i data-feather="inbox"></i></span><span className="badge badge-sm badge-soft-primary badge-sm badge-pill position-top-end-overflow-1">4</span></span></span></a>
+                                </li>
+                            
+                                <li className="nav-item">
+                                    <div className="dropdown dropdown-notifications">
+                                        <a href="#" className="btn btn-icon btn-rounded btn-flush-dark flush-soft-hover dropdown-toggle no-caret" data-bs-toggle="dropdown" data-dropdown-animation role="button" aria-haspopup="true" aria-expanded="false"><span className="icon"><span className="position-relative"><span className="feather-icon"><i data-feather="bell"></i></span><span className="badge badge-success badge-indicator position-top-end-overflow-1"></span></span></span></a>
+                                        <div className="dropdown-menu dropdown-menu-end p-0">
+                                            <h6 className="dropdown-header px-4 fs-6">Notifications<a href="#" className="btn btn-icon btn-flush-dark btn-rounded flush-soft-hover"><span className="icon"><span className="feather-icon"><i data-feather="settings"></i></span></span></a>
+                                            </h6>
+                                            <div data-simplebar className="dropdown-body  p-2">
+                                                <a href="#" className="dropdown-item">
+                                                    <div className="media">
+                                                        <div className="media-head">
+                                                            <div className="avatar avatar-rounded avatar-sm">
+                                                                <img src="dist/img/avatar2.jpg" alt="user" className="avatar-img"/>
+                                                            </div>
+                                                        </div>
+                                                        <div className="media-body">
+                                                            <div>
+                                                                <div className="notifications-text">Morgan Freeman accepted your invitation to join the team</div>
+                                                                <div className="notifications-info">
+                                                                    <span className="badge badge-soft-success">Collaboration</span>
+                                                                    <div className="notifications-time">Today, 10:14 PM</div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                                <a href="#" className="dropdown-item">
+                                                    <div className="media">
+                                                        <div className="media-head">
+                                                            <div className="avatar  avatar-icon avatar-sm avatar-success avatar-rounded">
+                                                                <span className="initial-wrap">
+                                                                    <span className="feather-icon"><i data-feather="inbox"></i></span>
+                                                                </span>
+                                                            </div>
+                                                        </div>
+                                                        <div className="media-body">
+                                                            <div>
+                                                                <div className="notifications-text">New message received from Alan Rickman</div>
+                                                                <div className="notifications-info">
+                                                                    <div className="notifications-time">Today, 7:51 AM</div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                                <a href="#" className="dropdown-item">
+                                                    <div className="media">
+                                                        <div className="media-head">
+                                                            <div className="avatar  avatar-icon avatar-sm avatar-pink avatar-rounded">
+                                                                <span className="initial-wrap">
+                                                                    <span className="feather-icon"><i data-feather="clock"></i></span>
+                                                                </span>
+                                                            </div>
+                                                        </div>
+                                                        <div className="media-body">
+                                                            <div>
+                                                                <div className="notifications-text">You have a follow up with Jampack Head on Friday, Dec 19 at 9:30 am</div>
+                                                                <div className="notifications-info">
+                                                                    <div className="notifications-time">Yesterday, 9:25 PM</div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                                <a href="#" className="dropdown-item">
+                                                    <div className="media">
+                                                        <div className="media-head">
+                                                            <div className="avatar avatar-sm avatar-rounded">
+                                                                <img src="dist/img/avatar3.jpg" alt="user" className="avatar-img"/>
+                                                            </div>
+                                                        </div>
+                                                        <div className="media-body">
+                                                            <div>
+                                                                <div className="notifications-text">Application of Sarah Williams is waiting for your approval</div>
+                                                                <div className="notifications-info">
+                                                                    <div className="notifications-time">Today 10:14 PM</div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                                <a href="#" className="dropdown-item">
+                                                    <div className="media">
+                                                        <div className="media-head">
+                                                            <div className="avatar avatar-sm avatar-rounded">
+                                                                <img src="dist/img/avatar10.jpg" alt="user" className="avatar-img"/>
+                                                            </div>
+                                                        </div>
+                                                        <div className="media-body">
+                                                            <div>	
+                                                                <div className="notifications-text">Winston Churchil shared a document with you</div>
+                                                                <div className="notifications-info">
+                                                                    <span className="badge badge-soft-violet">File Manager</span>
+                                                                    <div className="notifications-time">2 Oct, 2021</div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                                <a href="#" className="dropdown-item">
+                                                    <div className="media">
+                                                        <div className="media-head">
+                                                            <div className="avatar  avatar-icon avatar-sm avatar-danger avatar-rounded">
+                                                                <span className="initial-wrap">
+                                                                    <span className="feather-icon"><i data-feather="calendar"></i></span>
+                                                                </span>
+                                                            </div>
+                                                        </div>
+                                                        <div className="media-body">
+                                                            <div>	
+                                                                <div className="notifications-text">Last 2 days left for the project to be completed</div>
+                                                                <div className="notifications-info">
+                                                                    <span className="badge badge-soft-orange">Updates</span>
+                                                                    <div className="notifications-time">14 Sep, 2021</div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                            </div>
+                                            <div className="dropdown-footer"><a href="#"><u>View all notifications</u></a></div>
+                                        </div>
+                                    </div>
+                                </li>
+                                */}
+                                <li className="nav-item">
+                                    <div className="dropdown ps-2">
+                                        <a className=" dropdown-toggle no-caret" href="#" role="button" data-bs-display="static" data-bs-toggle="dropdown" data-dropdown-animation data-bs-auto-close="outside" aria-expanded="false">
+                                            <div className="avatar avatar-rounded avatar-xs">
+                                                <img src="/dist/img/avatar12.jpg" alt="user" className="avatar-img"/>
+                                            </div>
+                                        </a>
+                                        <div className={isActive ? 'dropdown-menu dropdown-menu-end show showSlow' : 'dropdown-menu dropdown-menu-end showSlow'}>
+                                            <div className="p-2">
+                                                <div className="media">
+                                                    <div className="media-head me-2">
+                                                        <div className="avatar avatar-primary avatar-sm avatar-rounded">
+                                                            <span className="initial-wrap">{}</span>
+                                                        </div>
+                                                    </div>
+                                                    <div className="media-body">
+                                                        <div className="fs-7">{}</div>
+                                                        <a href="/login" className="d-block fs-8 link-secondary" onClick={signOut}><u>Դուրս գալ</u></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+											{/*
+                                            <div className="dropdown-divider"></div>
+                                                <a className="dropdown-item" href="profile.html">Profile</a>
+                                                <a className="dropdown-item" href="/privacy-policy">
+                                                    <span className="me-2">Offers</span>
+                                                    <span className="badge badge-sm badge-soft-pink">2</span>
+                                                </a>
+                                                <div className="dropdown-divider"></div>
+                                                <h6 className="dropdown-header">Manage Account</h6>
+                                                <a className="dropdown-item" href="/privacy-policy"><span className="dropdown-icon feather-icon"><i data-feather="credit-card"></i></span><span>Payment methods</span></a>
+                                                <a className="dropdown-item" href="/privacy-policy"><span className="dropdown-icon feather-icon"><i data-feather="check-square"></i></span><span>Subscriptions</span></a>
+                                                <a className="dropdown-item" href="/privacy-policy"><span className="dropdown-icon feather-icon"><i data-feather="settings"></i></span><span>Settings</span></a>
+                                                <div className="dropdown-divider"></div>
+                                                <a className="dropdown-item" href="/privacy-policy"><span className="dropdown-icon feather-icon"><i data-feather="tag"></i></span><span>Raise a ticket</span></a>
+                                      <div className="dropdown-divider"></div>
+											*/}
+                                      <a className="dropdown-item" href="/">Օգնություն և սպասարկում</a>
+                                  </div>
+                              </div>
+                          </li>
+                      </ul>
+                  </div>
+                  {/* /End Nav */}
+                  </div>									
+              </nav>
               {/* Vertical Nav */}
               <div className="hk-menu">
               {/* Brand */}
