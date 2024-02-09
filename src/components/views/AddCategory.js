40281afde32fabeb13dca38ee8f3917048528456
Hayk Harutyunyan <haykprog0720@gmail.com>
2024-02-10 02:58:47 +0400

    add AddCategory Modal for ResearchList View

---
+++ src/components/views/AddCategory.js
@@ -0,0 +1,127 @@
+import FeatherIcon from 'feather-icons-react/build/FeatherIcon'
+import React from 'react'
+import { name_validation, shortName_validation } from '../../utils/inputValidations'
+import { Input } from '../Input'
+import { Form, FormProvider, useForm } from "react-hook-form";
+import { Modal } from "react-bootstrap";
+
+function AddCategory({handleToggleCategoryCreateModal}) {
+    const methods = useForm({
+        mode: "onChange",
+      });
+  return (
+    <>
+       <Modal
+        show={() => true}
+        size="xl"
+        onHide={() => handleToggleCategoryCreateModal(false)}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title style={{ width: "100%", textAlign: "center" }}>
+            Ավելացնել նոր Հետազոտություն
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <FormProvider {...methods}>
+            <div className="contact-body contact-detail-body">
+              <div data-simplebar className="nicescroll-bar">
+                <div className="d-flex flex-xxl-nowrap flex-wrap">
+                  <div className="contact-info w-100">
+                    <Form
+                      onSubmit={(e) => e.preventDefault()}
+                      noValidate
+                      autoComplete="off"
+                      className="container"
+                    >
+                      <div className="card">
+                        <div className="card-header">
+                          <a href="#">Հետազոտության տվյալներ</a>
+                          <button
+                            className="btn btn-xs btn-icon btn-rounded btn-light"
+                            data-bs-toggle="tooltip"
+                            data-bs-placement="top"
+                            title=""
+                            data-bs-original-title="Edit"
+                          >
+                            <span
+                              className="icon"
+                              data-bs-toggle="modal"
+                              data-bs-target="#editInfo"
+                            >
+                              <span class="feather-icon">
+                                <FeatherIcon icon="edit-2" />
+                              </span>
+                            </span>
+                          </button>
+                        </div>
+                        <div className="card-body">
+                          <div className="modal-body">
+                            <div className="row gx-3">
+                              <div className="col-sm-6">
+                                <Input {...name_validation} />
+                              </div>
+                              <div className="col-sm-6">
+                                <Input {...shortName_validation} />
+                              </div>
+                            </div>
+                            
+                            
+                          </div>
+                          </div>
+                          </div>
+                          <div className="separator-full"></div>
+                          <div className="card">
+                        <div className="card-header">
+                          <a href="#">Հավելյալ տվյալներ</a>
+                          <button
+                            className="btn btn-xs btn-icon btn-rounded btn-light"
+                            data-bs-toggle="tooltip"
+                            data-bs-placement="top"
+                            title=""
+                            data-bs-original-title="Edit"
+                          >
+                            <span
+                              class="icon"
+                              data-bs-toggle="modal"
+                              data-bs-target="#moreContact"
+                            >
+                              <span class="feather-icon">
+                                <FeatherIcon icon="edit-2" />
+                              </span>
+                            </span>
+                          </button>
+                        </div>
+                       
+                      </div>
+                      <div className="separator-full"></div>
+  
+                      <div className="modal-footer align-items-center">
+                        <button
+                          type="button"
+                          className="btn btn-secondary"
+                          onClick={() => handleToggleCategoryCreateModal(false)}
+                        >
+                          Չեղարկել
+                        </button>
+                        <button
+                          type="button"
+                         // onClick={onSubmit}
+                          className="btn btn-primary"
+                          data-bs-dismiss="modal"
+                        >
+                          Ավելացնել
+                        </button>
+                      </div>
+                    </Form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </FormProvider>
+        </Modal.Body>
+      </Modal>
+    </>
+  )
+}
+
+export default AddCategory
