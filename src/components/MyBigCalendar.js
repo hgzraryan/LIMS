6be1f3cbbd7b021125c1daa4c10924ec11300ee0
Hayk Harutyunyan <haykprog0720@gmail.com>
2024-04-28 03:58:31 +0400

    corrected data an functionality of Doctor's big calendar

--- src/components/MyBigCalendar.js
+++ src/components/MyBigCalendar.js
@@ -13,67 +13,58 @@ const localizer = momentLocalizer(moment);
 
 const MyBigCalendar = ({ events, setEvents }) => {
   const [showModal, setShowModal] = useState(false);
+  const [showCheckModal, setShowCheckModal] = useState(false);
   const [selectedEvent, setSelectedEvent] = useState(null);
-
-  // useEffect(() => {
-  //   fetchEvents();
-  // }, [fetchEvents]);
-
-  const renderEventContent = (slotInfo) => {
-    const date = moment(slotInfo.start).format("MMMM D, YYYY");
-    return (
-      <div>
-        <p>
-          Date: <strong>{date}</strong>
-        </p>
-        <p>Location: {slotInfo.location}</p>
-      </div>
-    );
-  };
 
   const handleModalClose = () => {
     setShowModal(false);
   };
-  const updateEvents = (values) => {
-    // console.log(values);
-    // console.log(events);
-
-    const index = events.findIndex(event => event.id === values.id);
-    if (index !== -1) {
-    const updatedevents = [...events];
-    updatedevents[index] = values;
-    setEvents(updatedevents);
-    //console.log(updatedevents);
-  } else {
-    //console.log("Event with specified id not found.",index);
-  }
+  const handleCheckModalClose = () => {
+    setShowCheckModal(false);
   };
+
+  const updateEvents = (values) => {
+    const updatedEvents = events.map((event) =>
+      event.id === values.id ? { ...event, ...values } : event
+    );
+    setEvents(updatedEvents);
+  };
+
   const handleModalSave = () => {
     if (selectedEvent) {
-      //console.log(selectedEvent);
-      //setEvents(selectedEvent)
-      updateEvents(selectedEvent);
-    } else {
-      //createEvent(selectedEvent);
+      if (selectedEvent.id) {
+        // Update existing event
+        updateEvents(selectedEvent);
+      } else {
+        // Generate a new id for the new event
+        const newEventId = Math.max(...events.map(event => event.id), 0) + 1;
+        const newEvent = { ...selectedEvent, id: newEventId };
+        setEvents([...events, newEvent]); 
+      }
     }
     setShowModal(false);
   };
 
+ 
   const handleModalOpen = (slotInfo) => {
-    //console.log("data",slotInfo)
+    slotInfo?.id ? setShowCheckModal(slotInfo) : setShowModal(true);
     setSelectedEvent(slotInfo);
-    setShowModal(true);
   };
 
+  const handleEditModalOpen = () => {
+    setShowModal(true);
+    setShowCheckModal(false);
+  };
   const handleDeleteEvent = (eventId) => {
-    // deleteEvent(eventId);
+    const updatedEvents = events.filter((event) => event.id !== eventId);
+    setEvents(updatedEvents);
     setShowModal(false);
+    setShowCheckModal(false);
   };
 
   const eventStyleGetter = (event, start, end, isSelected) => {
-
-    let current_time = moment().format("YYYY MM DD");
-    let event_time = moment(event.start).format("YYYY MM DD");
+    let current_time = moment.utc().format("YYYY MM DD HH:mm");
+    let event_time = moment.utc(event.start).format("YYYY MM DD HH:mm");
     let background = current_time > event_time ? "#DE6987" : "#8CBD4C";
     return {
       style: {
@@ -81,16 +72,13 @@ const MyBigCalendar = ({ events, setEvents }) => {
       },
     };
   };
-  //console.log(Calendar)
+
   return (
     <div className="calendar-container">
       <Calendar
         popup
         selectable
         localizer={localizer}
-        //defaultView={Views.MONTH}
-        //components={{ toolbar: CustomToolbar }}
-        //views={["month"]}
         style={{ height: 600 }}
         events={events}
         eventPropGetter={eventStyleGetter}
@@ -100,27 +88,17 @@ const MyBigCalendar = ({ events, setEvents }) => {
       <Modal show={showModal} onHide={handleModalClose} size="sm">
         <Modal.Header closeButton>
           <Modal.Title>
-            {selectedEvent ? "Update Event" : "Create Event"}
+            {selectedEvent?.id ? "Փոփոխել" : "Ստեղծել"}
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <div className="d-flex flex-column">
             <input
-              placeholder="Event Title"
+              placeholder="Անվանում"
               defaultValue={selectedEvent?.title}
-              onChange={(e)=>setSelectedEvent(e.target.value)}
-              style={{
-                margin: "2px",
-                border: "none",
-                borderRadius: "5px",
-                backgroundColor: "#e3e5e8",
-              }}
-            />
-            <input
-              placeholder="Event Location"
-              defaultValue={selectedEvent?.location}
-              onChange={(e)=>setSelectedEvent(e.target.value)}
-
+              onChange={(e) =>
+                setSelectedEvent({ ...selectedEvent, title: e.target.value })
+              }
               style={{
                 margin: "2px",
                 border: "none",
@@ -131,35 +109,49 @@ const MyBigCalendar = ({ events, setEvents }) => {
           </div>
         </Modal.Body>
         <Modal.Footer>
-          {selectedEvent && (
+          <Button variant="secondary" onClick={handleModalClose}>
+            Չեղարկել
+          </Button>
+          {selectedEvent?.id 
+          ? 
+          <Button variant="primary" onClick={handleModalSave}>
+            Հաստատել
+          </Button> 
+          : 
+          <Button variant="primary" onClick={handleModalSave}>
+          Ստեղծել
+          </Button>}
+          
+        </Modal.Footer>
+      </Modal>
+{/* Check Edit Modal */}
+      <Modal show={showCheckModal} onHide={handleCheckModalClose} size="sm">
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {showCheckModal.title}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="d-flex flex-column">
+            <div><span>Սկիզբ: </span><span className="fw-bold">{moment.utc(showCheckModal.start).format("DD-MM-YYYY  HH:mm")}</span></div>
+            <div> <span>Ավարտ: </span><span className="fw-bold">{moment.utc(showCheckModal.end).format("DD-MM-YYYY  HH:mm")}</span></div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>          
+          <Button variant="secondary" onClick={()=>handleEditModalOpen(selectedEvent)}>
+            Փոփոխել
+          </Button>
             <Button
               variant="danger"
-              onClick={() => handleDeleteEvent(selectedEvent.id)}
+              onClick={() => handleDeleteEvent(showCheckModal.id)}
             >
-              Delete
+              Ջնջել
             </Button>
-          )}
-          <Button variant="secondary" onClick={handleModalClose}>
-            Cancel
-          </Button>
-          <Button variant="primary" onClick={handleModalSave}>
-            Save
-          </Button>
+          
         </Modal.Footer>
       </Modal>
     </div>
   );
 };
-
-const mapStateToProps = (state) => ({
-  events: state.events,
-});
-
-// const mapDispatchToProps = (dispatch) => bindActionCreators({
-//   fetchEvents,
-//   createEvent,
-//   updateEvent,
-//   deleteEvent
-// }, dispatch);
 
 export default MyBigCalendar;
